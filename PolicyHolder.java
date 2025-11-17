493579a6eab24dca116e3388867d86f6f0dd1e5c
Tracy Uyen Le <ule00001@email.cpcc.edu>
2025-11-17 12:39:12 -0500

    Update PolicyHolder.java

--- PolicyHolder.java
+++ PolicyHolder.java
@@ -1,74 +1,70 @@
 public class PolicyHolder
 {
-    
-    private String firstName;
-    private String lastName;
-    private int age;
-    private String smokingStatus;   
-    private double height;
-    private double weight;
-
-    
-    public PolicyHolder()
-    {
-        firstName = "";
-        lastName = "";
-        age = 0;
-        smokingStatus = "non-smoker";
-        height = 0.0;
-        weight = 0.0;
-    }
-
-  
-    public PolicyHolder(PolicyHolder obj)
-    {
-        this.firstName = obj.firstName;
-        this.lastName = obj.lastName;
-        this.age = obj.age;
-        this.smokingStatus = obj.smokingStatus;
-        this.height = obj.height;
-        this.weight = obj.weight;
-    }
-
-    
-    public PolicyHolder(String firstName, String lastName, int age,
-                        String smokingStatus, double height, double weight)
-    {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.smokingStatus = smokingStatus;
-        this.height = height;
-        this.weight = weight;
-    }
-
-    
-    public String getFirstName() { return firstName; }
-    public String getLastName() { return lastName; }
-    public int getAge() { return age; }
-    public String getSmokingStatus() { return smokingStatus; }
-    public double getHeight() { return height; }
-    public double getWeight() { return weight; }
-
-    
-    public double getBMI()
-    {
-        return (weight * 703) / (height * height);
-    }
+   private String firstName;
+   private String lastName;
+   private int age;
+   private String smokingStatus;
+   private double height;  
+   private double weight;  
 
    
-    public String toString()
-    {
-        return String.format(
-            "Policyholder's First Name: %s\n" +
-            "Policyholder's Last Name: %s\n" +
-            "Policyholder's Age: %d\n" +
-            "Policyholder's Smoking Status (Y/N): %s\n" +
-            "Policyholder's Height: %.1f inches\n" +
-            "Policyholder's Weight: %.1f pounds\n" +
-            "Policyholder's BMI: %.2f",
-            firstName, lastName, age, smokingStatus,
-            height, weight, getBMI()
-        );
-    }
+   public PolicyHolder()
+   {
+      this.firstName = "";
+      this.lastName = "";
+      this.age = 0;
+      this.smokingStatus = "N";
+      this.height = 0.0;
+      this.weight = 0.0;
+   }
+
+   
+   public PolicyHolder(String firstName, String lastName, int age,
+                       String smokingStatus, double height, double weight)
+   {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.smokingStatus = smokingStatus;
+      this.height = height;
+      this.weight = weight;
+   }
+
+   
+   public PolicyHolder(PolicyHolder other)
+   {
+      this.firstName = other.firstName;
+      this.lastName = other.lastName;
+      this.age = other.age;
+      this.smokingStatus = other.smokingStatus;
+      this.height = other.height;
+      this.weight = other.weight;
+   }
+
+   public String getFirstName() { return firstName; }
+   public String getLastName() { return lastName; }
+   public int getAge() { return age; }
+   public String getSmokingStatus() { return smokingStatus; }
+   public double getHeight() { return height; }
+   public double getWeight() { return weight; }
+
+   
+   public double getBMI()
+   {
+      if (height == 0)
+         return 0;
+      return (weight * 703) / (height * height);
+   }
+
+   
+   public String toString()
+   {
+      return "Policyholder's First Name: " + firstName +
+             "\nPolicyholder's Last Name: " + lastName +
+             "\nPolicyholder's Age: " + age +
+             "\nPolicyholder's Smoking Status (Y/N): " + smokingStatus +
+             "\nPolicyholder's Height: " + height + " inches" +
+             "\nPolicyholder's Weight: " + weight + " pounds" +
+             String.format("\nPolicyholder's BMI: %.2f", getBMI());
+   }
 }
